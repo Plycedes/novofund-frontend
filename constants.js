43fb927c802cf087ddfdd2f78666fef113250e79
Plycedes <abhaypratapyadav340@gmail.com>
2023-06-27 00:53:34 +0530

    Fund, Withdraw and Balance button

---
+++ constants.js
@@ -0,0 +1,125 @@
+export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const abi = [
+    {
+        inputs: [
+            {
+                internalType: "address",
+                name: "priceFeedAddress",
+                type: "address",
+            },
+        ],
+        stateMutability: "nonpayable",
+        type: "constructor",
+    },
+    {
+        inputs: [],
+        name: "NovoFund_NotOwner",
+        type: "error",
+    },
+    {
+        stateMutability: "payable",
+        type: "fallback",
+    },
+    {
+        inputs: [],
+        name: "cheaperWithdraw",
+        outputs: [],
+        stateMutability: "payable",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "fund",
+        outputs: [],
+        stateMutability: "payable",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "i_owner",
+        outputs: [
+            {
+                internalType: "address",
+                name: "",
+                type: "address",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "minUSD",
+        outputs: [
+            {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [
+            {
+                internalType: "address",
+                name: "",
+                type: "address",
+            },
+        ],
+        name: "s_addressToAmountFunded",
+        outputs: [
+            {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [
+            {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+            },
+        ],
+        name: "s_funder",
+        outputs: [
+            {
+                internalType: "address",
+                name: "",
+                type: "address",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "s_priceFeed",
+        outputs: [
+            {
+                internalType: "contract AggregatorV3Interface",
+                name: "",
+                type: "address",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "withdraw",
+        outputs: [],
+        stateMutability: "payable",
+        type: "function",
+    },
+    {
+        stateMutability: "payable",
+        type: "receive",
+    },
+];
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import { ethers } from "./ethers-5.7.esm.min.js";
+import { abi, contractAddress } from "./constants.js";
 
 const connectButton = document.getElementById("connect-button");
 const fundButton = document.getElementById("fund-button");
+const withdrawButton = document.getElementById("withdraw-button");
+const balanceButton = document.getElementById("balance-button");
+
 connectButton.onclick = connect;
 fundButton.onclick = fund;
+withdrawButton.onclick = withdraw;
+balanceButton.onclick = balance;
 
 console.log(ethers);
 
@@ -18,12 +24,63 @@ async function connect() {
     }
 }
 
-async function fund(ethAmount) {
+async function fund() {
+    const ethAmount = document.getElementById("ethAmount").value;
     console.log(`Funding with ${ethAmount}`);
 
     if (typeof window.ethereum !== "undefined") {
-        const provider = new ethers.provider.Web3Provider(window.ethereum);
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
-        console.log(signer);
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+
+        try {
+            const transactionResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount),
+            });
+            await listenForTransactionMine(transactionResponse, provider);
+            console.log("Done!");
+        } catch (exception) {
+            console.log(exception);
+        }
+    }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}...`);
+
+    return new Promise((resolve, reject) => {
+        provider.once(transactionResponse.hash, (transactionReciept) => {
+            console.log(
+                `Completed with ${transactionReciept.confirmations} confirmations`
+            );
+            resolve();
+        });
+    });
+}
+
+async function withdraw() {
+    console.log("Withdrawing");
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+
+        const transactionResponse = await contract.withdraw();
+        await transactionResponse.wait(1);
+        console.log("Successful");
+    }
+}
+
+async function balance() {
+    console.log("Showing contract balance");
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+
+        let balance = await contract.provider.getBalance(contract.address);
+        balance /= 1000000000000000000;
+        //await transactionResponse.wait(1);
+        console.log(balance.toString());
     }
 }
